5d4e1e6696cb616d66a5f748fb01887524f7a851
lockade <jeffersondias-@hotmail.com>
2021-03-01 14:49:25 -0300

    f

--- Index.php
+++ Index.php
@@ -1,6 +1,6 @@
 <?php
 echo "Tentando...";
-$comando = escapeshellcmd('python3 /opt/lampp/htdocs/SiteTcc/ficheiro_python.py');
+$comando = escapeshellcmd('sudo python3 /opt/lampp/htdocs/SiteTcc/ficheiro_python.py');
 $cmdResult = shell_exec($comando);
 echo $cmdResult;
 
